768139e9bd89f4b11b5ca57e88b68f779d2077ce
sbattou <sofiane.battou@qlik.com>
2017-03-09 15:28:24 -0500

    Making a few UI and controller changes

--- script.js
+++ script.js
@@ -11,29 +11,28 @@
 				controller  : 'mainController'
 			})
 
-			// route for the about page
-			.when('/about', {
-				templateUrl : 'pages/about.html',
-				controller  : 'aboutController'
+			// route for the resume page
+			.when('/resume', {
+				templateUrl : 'pages/resume.html',
+				controller  : 'resumeController'
 			})
 
-			// route for the contact page
-			.when('/contact', {
-				templateUrl : 'pages/contact.html',
-				controller  : 'contactController'
+			// route for the review page
+			.when('/review', {
+				templateUrl : 'pages/review.html',
+				controller  : 'reviewController'
 			});
 	});
 
 	// create the controller and inject Angular's $scope
 	scotchApp.controller('mainController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
+		$scope.message = 'This is the home page!';
 	});
 
-	scotchApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
+	scotchApp.controller('resumeController', function($scope) {
+		$scope.message = 'This is the resume page!';
 	});
 
-	scotchApp.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
+	scotchApp.controller('reviewController', function($scope) {
+		$scope.message = 'This is the review page!';
 	});
