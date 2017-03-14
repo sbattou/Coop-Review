b294d4918d18d0fbfc7e258ebb5fc07b922474a0
sbattou <sofiane.battou@qlik.com>
2017-03-14 16:44:59 -0400

    Adding an Account & Login page

--- script.js
+++ script.js
@@ -3,36 +3,54 @@
 
 	// configure our routes
 	scotchApp.config(function($routeProvider) {
-		$routeProvider
+	    $routeProvider
 
-			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/home.html',
-				controller  : 'mainController'
-			})
+	        // route for the home page
+	        .when('/', {
+	            templateUrl: 'pages/home.html',
+	            controller: 'mainController'
+	        })
 
-			// route for the resume page
-			.when('/resume', {
-				templateUrl : 'pages/resume.html',
-				controller  : 'resumeController'
-			})
+	        // route for the resume page
+	        .when('/resume', {
+	            templateUrl: 'pages/resume.html',
+	            controller: 'resumeController'
+	        })
 
-			// route for the review page
-			.when('/review', {
-				templateUrl : 'pages/review.html',
-				controller  : 'reviewController'
-			});
+	        // route for the review page
+	        .when('/review', {
+	            templateUrl: 'pages/review.html',
+	            controller: 'reviewController'
+	        })
+
+	        // route for the account page
+	        .when('/account', {
+	            templateUrl: 'pages/account.html',
+	            controller: 'accountController'
+	        })
+
+	        // route for the login page
+	        .when('/login', {
+	            templateUrl: 'pages/login.html',
+	            controller: 'loginController'
+	        });
 	});
 
 	// create the controller and inject Angular's $scope
 	scotchApp.controller('mainController', function($scope) {
-		$scope.message = 'This is the home page!';
+	    $scope.message = 'This is the home page!';
 	});
 
 	scotchApp.controller('resumeController', function($scope) {
-		$scope.message = 'This is the resume page!';
+	    $scope.message = 'This is the resume page!';
 	});
 
 	scotchApp.controller('reviewController', function($scope) {
-		$scope.message = 'This is the review page!';
+	    $scope.message = 'This is the review page!';
 	});
+
+	scotchApp.controller('accountController', function($scope) {
+	    $scope.message = 'This is the account page!';
+	});
+
+	scotchApp.controller('loginController', function($scope) {});
